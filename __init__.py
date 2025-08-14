53d64ee2aa170fe794a4e3506e006d902209455c
James Chapman <79722245+chapmanj-2810@users.noreply.github.com>
2025-08-14 08:05:50 +0100

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,18 @@
+"""
+DataCamp Exercise Generator
+
+An intelligent system for automatically generating DataCamp exercises from video content.
+"""
+
+from core import LearningDesigner, load_video_content
+from generators import get_exercise_generator
+from models import ExerciseType
+
+__version__ = "0.1.0"
+
+__all__ = [
+    "LearningDesigner",
+    "get_exercise_generator", 
+    "load_video_content",
+    "ExerciseType"
+]
