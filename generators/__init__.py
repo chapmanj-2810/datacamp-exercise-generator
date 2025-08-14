8bcc5055028ee03c6207d656a3649edcb8d79c41
James Chapman <79722245+chapmanj-2810@users.noreply.github.com>
2025-08-14 17:10:36 +0100

    Add drag and drop classify support

--- generators/__init__.py
+++ generators/__init__.py
@@ -7,11 +7,13 @@ This package contains exercise generators for different exercise types.
 from .base import ExerciseGenerator
 from .single_mcq import SingleAnswerMCQGenerator
 from .multiple_mcq import MultipleAnswerMCQGenerator
+from .drag_drop_classify import DragDropClassifyGenerator
 from .factory import get_exercise_generator
 
 __all__ = [
     "ExerciseGenerator",
     "SingleAnswerMCQGenerator",
-    "MultipleAnswerMCQGenerator", 
+    "MultipleAnswerMCQGenerator",
+    "DragDropClassifyGenerator",
     "get_exercise_generator"
 ]
