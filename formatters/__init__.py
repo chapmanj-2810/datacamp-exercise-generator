cc4ff9aa3a18c7dadd487dfcddc7e0fd1cf13e97
James Chapman <79722245+chapmanj-2810@users.noreply.github.com>
2025-08-14 17:10:00 +0100

    Add drag and drop classify support

--- formatters/__init__.py
+++ formatters/__init__.py
@@ -7,9 +7,11 @@ This package contains formatters that convert exercise objects to markdown/YAML 
 from .base import ExerciseFormatter
 from .single_mcq import SingleAnswerMCQFormatter
 from .multiple_mcq import MultipleAnswerMCQFormatter
+from .drag_drop_classify import DragDropClassifyFormatter
 
 __all__ = [
     "ExerciseFormatter",
     "SingleAnswerMCQFormatter",
-    "MultipleAnswerMCQFormatter"
+    "MultipleAnswerMCQFormatter",
+    "DragDropClassifyFormatter"
 ]
