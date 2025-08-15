9fbcfd2649d4f213515fa27ee8337ce2c4818abb
James Chapman <79722245+chapmanj-2810@users.noreply.github.com>
2025-08-15 15:15:16 +0100

    add hint back and remove comments

--- generators/factory.py
+++ generators/factory.py
@@ -2,22 +2,23 @@
 Factory function for creating exercise generators.
 """
 
+from typing import Any
 from .base import ExerciseGenerator
 from .single_mcq import SingleAnswerMCQGenerator
 from .multiple_mcq import MultipleAnswerMCQGenerator
 from .drag_drop_classify import DragDropClassifyGenerator
 from .drag_drop_order import DragDropOrderGenerator
-from .coding import CodingGenerator  # New import
+from .coding import CodingGenerator
 
 
-def get_exercise_generator(exercise_type: str = "single_mcq", **kwargs) -> ExerciseGenerator:
+def get_exercise_generator(exercise_type: str = "single_mcq", **kwargs: Any) -> ExerciseGenerator:
     """Factory function to get the appropriate exercise generator."""
     generators: dict[str, type[ExerciseGenerator]] = {
         "single_mcq": SingleAnswerMCQGenerator,
         "multiple_mcq": MultipleAnswerMCQGenerator,
         "drag_drop_classify": DragDropClassifyGenerator,
         "drag_drop_order": DragDropOrderGenerator,
-        "coding": CodingGenerator,  # New generator
+        "coding": CodingGenerator,
     }
     
     if exercise_type not in generators:
