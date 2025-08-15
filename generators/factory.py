5f76fbd745af62ec815512751cccf5985bf941a4
James Chapman <79722245+chapmanj-2810@users.noreply.github.com>
2025-08-15 14:44:04 +0100

    add support for coding exercises

--- generators/factory.py
+++ generators/factory.py
@@ -2,21 +2,22 @@
 Factory function for creating exercise generators.
 """
 
-from typing import Any
 from .base import ExerciseGenerator
 from .single_mcq import SingleAnswerMCQGenerator
 from .multiple_mcq import MultipleAnswerMCQGenerator
 from .drag_drop_classify import DragDropClassifyGenerator
 from .drag_drop_order import DragDropOrderGenerator
+from .coding import CodingGenerator  # New import
 
 
-def get_exercise_generator(exercise_type: str = "single_mcq", **kwargs: Any) -> ExerciseGenerator:
+def get_exercise_generator(exercise_type: str = "single_mcq", **kwargs) -> ExerciseGenerator:
     """Factory function to get the appropriate exercise generator."""
     generators: dict[str, type[ExerciseGenerator]] = {
         "single_mcq": SingleAnswerMCQGenerator,
         "multiple_mcq": MultipleAnswerMCQGenerator,
         "drag_drop_classify": DragDropClassifyGenerator,
         "drag_drop_order": DragDropOrderGenerator,
+        "coding": CodingGenerator,  # New generator
     }
     
     if exercise_type not in generators:
