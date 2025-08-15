364efa487bc946e08e5714ce25f1806bf4e32ad5
James Chapman <79722245+chapmanj-2810@users.noreply.github.com>
2025-08-15 15:24:03 +0100

    revert changes

--- models/__init__.py
+++ models/__init__.py
@@ -4,7 +4,11 @@ DataCamp Exercise Generator - Models Package
 This package contains all Pydantic models and data structures used throughout the system.
 """
 
-from .exercises import Exercise, SingleAnswerMCQExercise, MultipleAnswerMCQExercise, CodingExercise
+from .exercises import (
+    Exercise, SingleAnswerMCQExercise, MultipleAnswerMCQExercise,
+    DragDropClassifyExercise, DragDropOrderExercise, CodingExercise,
+    DraggableItem, DropZone, OrderableItem
+)
 from .planning import ExerciseType, ExercisePlan, LearningPlan
 from .examples import EXERCISE_EXAMPLES
 
@@ -12,7 +16,12 @@ __all__ = [
     "Exercise",
     "SingleAnswerMCQExercise", 
     "MultipleAnswerMCQExercise",
+    "DragDropClassifyExercise",
+    "DragDropOrderExercise",
     "CodingExercise",
+    "DraggableItem",
+    "DropZone", 
+    "OrderableItem",
     "ExerciseType",
     "ExercisePlan",
     "LearningPlan",
